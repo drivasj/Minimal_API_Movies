9a7b464ce06f995557127be2fd016f11b2cd1550
[Daniel Rj] <[darivasjt@gmail.com]>
2025-10-15 15:10:31 -0400

    Save image

--- MinimalApiMovies/Endpoints/ActoresEndpoints.cs
+++ MinimalApiMovies/Endpoints/ActoresEndpoints.cs
@@ -5,11 +5,13 @@ using Microsoft.AspNetCore.OutputCaching;
 using MinimalApiMovies.DTOs.Actores;
 using MinimalApiMovies.Entities;
 using MinimalApiMovies.Repositorys.Interface;
+using MinimalApiMovies.Services.Interface;
 
 namespace MinimalApiMovies.Endpoints
 {
     public static class ActoresEndpoints
     {
+        private static readonly string containerName = "actores";
         public static RouteGroupBuilder MapActores(this RouteGroupBuilder groupBuilder)
         {
             groupBuilder.MapPost("/", Create).DisableAntiforgery();
@@ -19,10 +21,19 @@ namespace MinimalApiMovies.Endpoints
         public static async Task<Created<ActorDTO>> Create([FromForm] CreateActorDTO createActorDTO, 
             IRepositoryActores repositoryActores,
             IOutputCacheStore outputCacheStore,
-            IMapper mapper
+            IMapper mapper,
+            IAlmacenadorArchivos almacenadorArchivos
             )
         {
             var actor = mapper.Map<Actor>(createActorDTO);
+
+            if(createActorDTO.Foto is not null)
+            {
+                var url = await almacenadorArchivos.Save(containerName, createActorDTO.Foto);
+                actor.Foto = url;
+            }
+
+
             var id = await repositoryActores.CreateActor(actor);
             await outputCacheStore.EvictByTagAsync("actores-get", default);
             var actorDTO = mapper.Map<ActorDTO>(actor);
---
+++ MinimalApiMovies/Services/Interface/IAlmacenadorArchivos.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalApiMovies.Services.Interface
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task Delete(string? route, string container);
+        Task<string> Save(string container, IFormFile file);
+        async Task<string> Edit(string route, string container, IFormFile file)
+        {
+            // If the route is not null or empty, delete the existing file in azure
+            await Delete(route, container); return await Save(container, file);
+        }
+    }
+}
